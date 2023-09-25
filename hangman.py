bc0c312c3f1f9f4b7444e76b6dd1691d48ab8a1e
JoaoF17 <joao_fernandess17@hotmail.com>
2023-09-25 17:16:05 +0700

    improve hangman, moved non logic data to a separate file called hangman_data

--- hangman.py
+++ hangman.py
@@ -1,58 +1,10 @@
 import random
+import hangman_data
 
 #step 1 
 
 word_list = ['alphabet', 'programing', 'football']
-HANGMANPICS = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''']
+
 
 #TODO 1 - Randomly choose a word from the word_list and assign it to a var called chosen_word. Create a display var that contains as many _ as the number of letters in the word.
 
@@ -69,32 +21,35 @@ for letter in chosen_word:
 guess = ''
 lifes = 6
 end_game = False
-print('Welcome to Hangman, enjoy!')
-print(HANGMANPICS[lifes] + f'\nYou start with {lifes} lifes.')
+print(hangman_data.logo)
+print(hangman_data.HANGMANPICS[lifes] + f'\nYou start with {lifes} lifes.')
 
 while not end_game:
   #Logic to make sure player only inputs one character.
   while True :
     guess = input("Guess a letter: \n").lower()
     if len(guess) == 1:
-      print(f'Your single character input was: {guess}')
+      print(f'Your character input was: {guess}')
       break
     print("Please enter a single character to continue.\n")
 
-  #TODO - Loop through each position in the chosen_word, if the given letter matches with any letter in the word, swap the blank space for the letter in the right position.
-
-  for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-      display[position] = letter
-  print(display)
+  
 
   #Manage player lifes
   if guess not in chosen_word:
     lifes -= 1
-  print(lifes)
+    print(f"The hidden word does not contain the letter chosen.\nYou have {lifes} lifes left.")
   #print hangman depending on how many lifes player has
-  print(HANGMANPICS[lifes])
+  print(hangman_data.HANGMANPICS[lifes])
+
+#Loop through each position in the chosen_word, if the given letter matches with any letter in the word, swap the blank space for the letter in the right position.
+  for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if letter == guess:
+      display[position] = letter
+      print("Good choice!")
+  print(display)
+
   #end the game, by either losing all lifes, or completing the word
   if lifes < 1:
     end_game = True
