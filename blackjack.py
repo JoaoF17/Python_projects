ea8500398fa873f6096ccf5509f2ca069f3107c2
JoaoF17 <joao_fernandess17@hotmail.com>
2023-10-16 23:06:23 +0700

    update rules of the game

--- blackjack.py
+++ blackjack.py
@@ -3,7 +3,7 @@ import random
 cards = ["A", "K", "J", "Q", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
 
 card_points = {
-  "A" : 11 or 1,
+  "A" : 11,
   "K" : 10,
   "J" : 10,
   "Q" : 10,
@@ -23,34 +23,64 @@ def random_number():
   r = random.randint(0, len(cards)-1)
   return r
 
-#player hand, 2 random cards
+#player hand, 2 random cards and points
 p1 = random_number()
 p2 = random_number()
 player = [cards[p1], cards[p2]]
 
-#logic to count points
-player_points = 0
-for point in player:
-  single_card_points = card_points[point]
-  player_points += single_card_points
-
 #dealer hand, 1 random card, and a facing down card
 d1 = random_number()
-dealer = [cards[d1], "_"]
+dealer = [cards[d1]]
 
-#logic to count points for computer
-computer_points = 0
-for point in dealer:
-  if point == "_":
-    single_card_points = 0
-  else:
-    single_card_points = card_points[point]
-  computer_points += single_card_points
-
-#function for player to draw a card
-def draw_card():
+#functions for player and computer to draw a card
+def player_draw_card():
   player.append(cards[random_number()])
 
+def dealer_draw_card():
+  dealer.append(cards[random_number()])
 
-print(f"Your hand: {player} Points: {player_points}")
-print(f"Dealer hand: {dealer} Points: {computer_points}")
+game_on = True
+while game_on:
+  #logic to count points for player
+  player_points = 0
+  for point in player:
+    single_card_points = card_points[point]
+    player_points += single_card_points
+    
+  #logic to count points for dealer
+  dealer_points = 0
+  for point in dealer:
+    single_card_points = card_points[point]
+    dealer_points += single_card_points
+    
+  print(f"Your hand: {player} Points: {player_points}")
+  print(f"Dealer hand: {dealer} Points: {dealer_points}")
+  
+  player_prompt = input("Would you like to draw a card? 'Y' or 'N' \n").lower()
+  
+  if player_prompt == "y":
+    player_draw_card()
+    #recalculate points to make sure it's not over 21
+    for point in player:
+      single_card_points = card_points[point]
+      player_points += single_card_points
+    if player_points > 21:
+      print("You went over, you lost.")
+      game_on = False
+      
+  elif player_prompt == "n":
+    while dealer_points < 17:
+      dealer_draw_card()
+      for point in dealer:
+        single_card_points = card_points[point]
+        dealer_points += single_card_points
+      if dealer_points > 21:
+        print("Dealer went over, YOU WON!")
+      elif dealer_points > player_points:
+        print("Dealer won!")
+        game_on = False
+  elif dealer_points > player_points:
+    print("Dealer won!")
+  
+  print(f"Your hand: {player} Points: {player_points}")
+  print(f"Dealer hand: {dealer} Points: {dealer_points}")
